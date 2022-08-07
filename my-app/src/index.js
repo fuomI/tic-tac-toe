68f6ef273da4dc2b2f836629a2f3c9480d868dc0
Joni Jansson <joni.jansson@saunalahti.fi>
2022-08-07 15:32:35 +0300

    added a boolean state 'xIsNext', handleClick() flips the value of 'xIsNext' to allow alternating between 'X' & 'O'

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,16 +17,22 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
+
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
+
       return (
         <Square
             value={this.state.squares[i]}
@@ -36,7 +42,8 @@ class Board extends React.Component {
     }
 
     render() {
-      const status = 'Next player: X';
+
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
       return (
         <div>
@@ -62,7 +69,9 @@ class Board extends React.Component {
   }
 
   class Game extends React.Component {
+
     render() {
+
       return (
         <div className="game">
           <div className="game-board">
