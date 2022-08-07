de0fb117284745fa457080e92d1d1558612ad0f4
Joni Jansson <joni.jansson@saunalahti.fi>
2022-08-07 12:36:31 +0300

    added the passing of props to squares, added onclick function to squares

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,16 +5,17 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={function() {
+        console.log('click'); }}>
+          {this.props.value}
         </button>
       );
     }
   }
 
-  class Board extends React.Component {
+class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
 
     render() {
